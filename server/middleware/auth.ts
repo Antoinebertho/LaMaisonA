5b8223501b8f0acdb83669dda22566b698e204e4
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-04-19 17:46:15 +0200

    added auth & admin in middleware

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,17 +1,43 @@
 import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import config from 'config';
+import User from '../models/userModel';
 
-export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.header('x-auth-token');
-    if (!token) {
-        return res.status(401).json({ msg: 'No token, authorization denied' });
-    }
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const decoded = jwt.verify(token, config.ACCESS_TOKEN);
-        req.user = decoded.user;
+        const token = req.header('x-auth-token');
+        if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+        const decoded = jwt.verify(token, config.ACCESS_TOKEN_SECRET);
+        req.user = await User.findById(decoded.id);
         next();
-    } catch (err) {
+}
+    catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' });
+    }
+}
+
+export const isAdmin = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.header('x-auth-token');
+        if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+        const decoded = jwt.verify(token, config.ACCESS_TOKEN_SECRET);
+        req.user = await User.findById(decoded.id);
+        next();
+    }
+    catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' });
+    }
+}
+
+export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.header('x-auth-token');
+        if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+        const decoded = jwt.verify(token, config.ACCESS_TOKEN_SECRET);
+        req.user = await User.findById(decoded.id);
+        next();
+    }
+    catch (err) {
         res.status(401).json({ msg: 'Token is not valid' });
     }
 }
