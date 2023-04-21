983cbf968b3a2b80bde1bbb7a68646a58e878227
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-04-21 15:07:54 +0200

    fixing auth

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -2,42 +2,41 @@ import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import config from 'config';
 import User from '../models/userModel';
+import {AuthenticatedRequest} from '../interfaces/authInterface';
 
-export const auth = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const token = req.header('x-auth-token');
-        if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
-        const decoded = jwt.verify(token, config.ACCESS_TOKEN_SECRET);
-        req.user = await User.findById(decoded.id);
-        next();
-}
-    catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
-    }
-}
 
-export const isAdmin = async (req: Request, res: Response, next: NextFunction) => {
+export const auth = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     try {
-        const token = req.header('x-auth-token');
-        if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
-        const decoded = jwt.verify(token, config.ACCESS_TOKEN_SECRET);
-        req.user = await User.findById(decoded.id);
-        next();
+      const token = req.header('x-auth-token');
+      if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+      const decoded = jwt.verify(token, config.ACCESS_TOKEN_SECRET);
+      req.user = await User.findById(decoded.id);
+      next();
+    } catch (err) {
+      res.status(401).json({ msg: 'Token is not valid' });
     }
-    catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
-    }
-}
+  }
 
-export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+  export const isAdmin = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     try {
-        const token = req.header('x-auth-token');
-        if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
-        const decoded = jwt.verify(token, config.ACCESS_TOKEN_SECRET);
-        req.user = await User.findById(decoded.id);
-        next();
+      const token = req.header('x-auth-token');
+      if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+      const decoded = jwt.verify(token, config.ACCESS_TOKEN_SECRET);
+      req.user = await User.findById(decoded.id);
+      next();
+    } catch (err) {
+      res.status(401).json({ msg: 'Token is not valid' });
     }
-    catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
+  }
+
+  export const verifyToken = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    try {
+      const token = req.header('x-auth-token');
+      if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+      const decoded = jwt.verify(token, config.ACCESS_TOKEN_SECRET);
+      req.user = await User.findById(decoded.id);
+      next();
+    } catch (err) {
+      res.status(401).json({ msg: 'Token is not valid' });
     }
-}
+  }
